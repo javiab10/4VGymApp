aa1d17591ef259a695e39bc36d4bfaa0d1d13099
Lxx367 <91671727+Lxx367@users.noreply.github.com>
2025-01-25 11:57:56 +0100

    Componente Monitores creado
    - Este componente se corresponde con la página monitores

---
+++ src/app/monitores/monitores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monitores',
+  standalone: true,
+  imports: [],
+  templateUrl: './monitores.component.html',
+  styleUrl: './monitores.component.scss'
+})
+export class MonitoresComponent {
+
+}
