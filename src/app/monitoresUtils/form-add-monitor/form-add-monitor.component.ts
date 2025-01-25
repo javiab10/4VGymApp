06a9bc07aec78178d4dba1cf6004e70e595e51f8
Lxx367 <91671727+Lxx367@users.noreply.github.com>
2025-01-25 12:30:28 +0100

    Crear componente Form-Add-Monitor
    Creación del componente form add monitor dentro de monitoresUtils

---
+++ src/app/monitoresUtils/form-add-monitor/form-add-monitor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-add-monitor',
+  imports: [],
+  templateUrl: './form-add-monitor.component.html',
+  styleUrl: './form-add-monitor.component.scss'
+})
+export class FormAddMonitorComponent {
+
+}
