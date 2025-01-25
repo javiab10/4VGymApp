10b13a4ca0da7a9bdaaafb505fa3586f9c9cad35
javiab10 <javierbasoko@gmail.com>
2025-01-25 12:06:50 +0100

    Función cambiar de página añadida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from "./footer/footer.component";
+import { ActivitiesComponent } from './activities/activities.component';
+import { MonitoresComponent } from './monitores/monitores.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ActivitiesComponent, MonitoresComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
