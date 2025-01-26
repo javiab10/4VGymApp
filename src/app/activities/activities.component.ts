aa56d88ac993676936306924b04cf04c91a4bdff
Lxx367 <91671727+Lxx367@users.noreply.github.com>
2025-01-26 11:59:22 +0100

    Actualización del calendario
    Actualización de angular materials para la funcionalidad del calendario

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -3,15 +3,14 @@ import { CommonModule, formatDate } from '@angular/common';
 import { FechaComponent } from "../activitiesUtils/fecha/fecha.component";
 import { FormsModule } from '@angular/forms';
 import { ActivityCardComponent } from "../activitiesUtils/activity-card/activity-card.component";
-// import { ModalAddActivityComponent } from "../activitiesUtils/modal-add-activity/modal-add-activity.component";
-// import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatNativeDateModule } from '@angular/material/core';
-// import { MatInputModule } from '@angular/material/input';
+ import { ModalAddActivityComponent } from "../activitiesUtils/modal-add-activity/modal-add-activity.component";
+ import { MatDatepickerModule } from '@angular/material/datepicker';
+ import { MatNativeDateModule } from '@angular/material/core';
+ import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-activities',
-  imports: [CommonModule, FechaComponent,FormsModule, ActivityCardComponent ],
-  // imports: [CommonModule, FechaComponent, FormsModule, ActivityCardComponent, ModalAddActivityComponent, MatDatepickerModule, MatNativeDateModule, MatInputModule],
+  imports: [CommonModule, FechaComponent, FormsModule, ActivityCardComponent, ModalAddActivityComponent, MatDatepickerModule, MatNativeDateModule, MatInputModule],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
