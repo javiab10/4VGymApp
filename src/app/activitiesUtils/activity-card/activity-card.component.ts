77223721e22f12156c86f1e13af91a3294349a53
javiab10 <javierbasoko@gmail.com>
2025-01-26 12:00:25 +0100

    Estilos de la aplicación actualizados

--- src/app/activitiesUtils/activity-card/activity-card.component.ts
+++ src/app/activitiesUtils/activity-card/activity-card.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, } from '@angular/core';
 import { ActivityService } from '../../services/activity.service';
 import { ActivityMonitorComponent } from "../activity-monitor/activity-monitor.component";
-// import { ActivityOfActivityComponent } from "../activity-of-activity/activity-of-activity.component";
+import { ActivityOfActivityComponent } from "../activity-of-activity/activity-of-activity.component";
 // import { ModalAddActivityComponent } from "../modal-add-activity/modal-add-activity.component";
-// import { ModalEditActivityComponent } from "../modal-edit-activity/modal-edit-activity.component";
+import { ModalEditActivityComponent } from "../modal-edit-activity/modal-edit-activity.component";
 
 @Component({
   selector: 'app-activity-card',
-  imports: [ActivityMonitorComponent],
+  imports: [ActivityMonitorComponent, ActivityOfActivityComponent, ModalEditActivityComponent],
   // imports: [ActivityMonitorComponent, ActivityOfActivityComponent, ModalAddActivityComponent, ModalEditActivityComponent],
   templateUrl: './activity-card.component.html',
   styleUrl: './activity-card.component.scss'
