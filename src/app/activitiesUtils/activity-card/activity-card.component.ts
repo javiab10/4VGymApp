12c8b6722a6f5baec4fbbd0629761ba8eb953bb0
javiab10 <javierbasoko@gmail.com>
2025-01-26 11:41:38 +0100

    1ª Versión del Activity-Card Component
    - Cada Activity Card mostrará los detalles de la actividad: Monitor 1, Monitor 2 y Tipo de actividad. También podremos eliminarla o editarla si es que la hay, o agregar una nueva

--- src/app/activitiesUtils/activity-card/activity-card.component.ts
+++ src/app/activitiesUtils/activity-card/activity-card.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, } from '@angular/core';
+import { ActivityService } from '../../services/activity.service';
+import { ActivityMonitorComponent } from "../activity-monitor/activity-monitor.component";
+// import { ActivityOfActivityComponent } from "../activity-of-activity/activity-of-activity.component";
+// import { ModalAddActivityComponent } from "../modal-add-activity/modal-add-activity.component";
+// import { ModalEditActivityComponent } from "../modal-edit-activity/modal-edit-activity.component";
 
 @Component({
   selector: 'app-activity-card',
-  imports: [],
+  imports: [ActivityMonitorComponent],
+  // imports: [ActivityMonitorComponent, ActivityOfActivityComponent, ModalAddActivityComponent, ModalEditActivityComponent],
   templateUrl: './activity-card.component.html',
   styleUrl: './activity-card.component.scss'
 })
 export class ActivityCardComponent {
+  @Input() date: Date = new Date();
+  @Input() startingTime: string = '';
+  @Input() endingTime: string = '';
+  activity: any;
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.loadActivity();
+  }
+
+  ngOnChanges(): void {
+    this.loadActivity();
+  }
+  
+  loadActivity() {
+    this.date = new Date(this.date); //Cuando recibimos la fecha del datepicker la recibimos como un string. Así que lo convertimos a fecha
+    this.activity = this.activityService.getActivityByDateAndStartingTime(this.date, this.startingTime);
+  }
+
+  onDeleteActivity(id: number): void {
+    this.activityService.deleteActivity(id);
+    this.loadActivity();
+  }
+
+  onActivityAdded(): void {
+    this.loadActivity();
+  }
 
 }
+
