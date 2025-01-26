52eff6076702735e09352152d88c17befd61d2cd
javiab10 <javierbasoko@gmail.com>
2025-01-26 11:44:42 +0100

    Componente Fecha
    - Un carrusel que nos permitirá ir desplazándonos por la fecha mostrando las actividades que corresponden a dicha fecha

---
+++ src/app/activitiesUtils/fecha/fecha.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+// import { FormatDatePipe } from "../../pipes/format-date.pipe";
+
+@Component({
+  selector: 'app-fecha',
+  // imports: [],
+  templateUrl: './fecha.component.html',
+  styleUrl: './fecha.component.scss'
+})
+export class FechaComponent {
+  @Input() date: Date = new Date();
+  @Output() dateChange = new EventEmitter<Date>();
+  todayDate: Date = new Date();
+
+  ngOnChanges(){
+    
+  }
+  prevDate(){
+    this.date = new Date(this.date);
+    this.date = new Date(this.date.getTime());
+    this.date.setDate(this.date.getDate()-1);
+
+    this.dateChange.emit(this.date);
+  }
+
+  nextDate(){
+    this.date = new Date(this.date);
+    this.date = new Date(this.date.getTime());
+    this.date.setDate(this.date.getDate()+1);
+
+    this.dateChange.emit(this.date);
+  }
+}
